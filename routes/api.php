e2607ccb4ca35719c40e34b92fdd2912c5ca6fd1
Kauan Santiago <kauan.santiago@alunos.unis.edu.br>
2026-04-23 19:53:18 -0300

    Removendo conflito para conseguir identificar as rotas

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/me', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
